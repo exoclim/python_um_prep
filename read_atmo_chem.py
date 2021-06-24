8aa54e12614eeb81eede3fca7a6af194c64723fe
Duncan Christie <duncanchristie@gmail.com>
2021-06-24 11:34:27 +0100

    Fixed unit conversion problem in read_atmo_chem()

--- read_atmo_chem.py
+++ read_atmo_chem.py
@@ -8,7 +8,7 @@ def read_atmo_chem(fname):
   data = nc.Dataset(fname)
 
   #Get pressure, abundances and molecule names
-  pressure   = data.variables['pressure'][:]*10. # Convert to Pa 
+  pressure   = data.variables['pressure'][:]*0.1 # Convert to Pa 
   abundances = data.variables['abundances'][:,:]
   names      = data.variables['molname'][:]
 
@@ -20,7 +20,7 @@ def read_atmo_chem_cpmmw(fname):
   data = nc.Dataset(fname)
 
   #Get pressure, cp and mean molecular weight
-  pressure   = data.variables['pressure'][:]*10. # Convert to Pa 
+  pressure   = data.variables['pressure'][:]*0.1 # Convert to Pa 
   cp         = data.variables['cp'][:]
   mmw        = data.variables['mean_mol_mass'][:]
 
